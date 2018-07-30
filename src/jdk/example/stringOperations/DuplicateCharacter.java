fd57d3e878f74ca9b7c119f0e146d6ca0629ca2c
unknown <sen.vikram5@gmail.com>
2018-07-30 20:30:51 +0530

    Adding some more logical program

---
+++ src/jdk/example/stringOperations/DuplicateCharacter.java
@@ -0,0 +1,24 @@
+package jdk.example.stringOperations;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateCharacter {
+public static void main(String[] args) {
+	System.out.println("Enter any String");
+	Scanner sc=new Scanner(System.in);
+	String input=sc.nextLine();
+	
+	char[] charArray=input.toCharArray();
+	Map<Character, Integer> charMap=new HashMap<>();
+	for(char c : charArray) {
+		if(charMap.containsKey(c)) {
+			charMap.put(c, charMap.get(c)+1);
+		}else {
+			charMap.put(c, 1);
+		}
+	}
+	charMap.forEach((k,v)->System.out.println("Frequency of: "+k+"="+v));
+}
+}
---
+++ src/jdk/example/stringOperations/DuplicateWord.java
@@ -0,0 +1,27 @@
+package jdk.example.stringOperations;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateWord {
+	public static void main(String[] args) {
+		System.out.println("Enter any String");
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+
+		String[] dupWord = input.split(" ");
+
+		Map<String, String> wordMap = new HashMap<>();
+		for (String word : dupWord) {
+			word.toUpperCase();
+			if (wordMap.get(word) != null) {
+				System.out.println("Duplicate word: " + word);
+			} else {
+				wordMap.put(word, word);
+			}
+		}
+		wordMap.forEach((k, v) -> System.out.print(v+" "));
+	}
+}
